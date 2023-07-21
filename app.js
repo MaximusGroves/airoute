a9b04f6a18276fefdb694c13d9decda5ae58aed4
Maximus Groves <maximus@vibewar.com>
2023-07-21 02:03:09 -0400

    adds sentiment

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+process.env['GOOGLE_APPLICATION_CREDENTIALS'] =
+  './thing1/vibewar-service-worker';
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -25,12 +28,12 @@ app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -44,6 +44,42 @@ router.post('/ear', upload.single('file'), async (req, res) => {
   }
 });
 
+router.post('/feelings', async (req, res) => {
+  // Imports the Google Cloud client library
+  const language = require('@google-cloud/language');
+
+  // Creates a client
+  const client = new language.LanguageServiceClient();
+
+  /**
+   * TODO(developer): Uncomment the following line to run this code.
+   */
+  const text = req?.body?.text || 'Your text to analyze, e.g. Hello, world!';
+
+  // Prepares a document, representing the provided text
+  const document = {
+    content: text,
+    type: 'PLAIN_TEXT',
+  };
+
+  // Detects the sentiment of the document
+  const [result] = await client.analyzeSentiment({ document });
+
+  const sentiment = result.documentSentiment;
+  console.log('Document sentiment:');
+  console.log(`  Score: ${sentiment.score}`);
+  console.log(`  Magnitude: ${sentiment.magnitude}`);
+
+  const sentences = result.sentences;
+  sentences.forEach((sentence) => {
+    console.log(`Sentence: ${sentence.text.content}`);
+    console.log(`  Score: ${sentence.sentiment.score}`);
+    console.log(`  Magnitude: ${sentence.sentiment.magnitude}`);
+  });
+
+  res.send(result);
+});
+
 router.post('/brain', async (req, res) => {
   const messages = req?.body?.messages || [
     {
